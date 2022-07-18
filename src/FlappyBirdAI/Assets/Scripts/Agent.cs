8695e693ce9f55e1c4cba30cd7c4d38d99d65794
vladimir-siv <vladimir.sivcev@gmail.com>
2022-07-18 18:55:20 +0200

    Random Agent

--- src/FlappyBirdAI/Assets/Scripts/Agent.cs
+++ src/FlappyBirdAI/Assets/Scripts/Agent.cs
@@ -1,56 +1,16 @@
 ﻿using UnityEngine;
-using GrandIntelligence;
 
 public sealed class Agent
 {
-	private readonly float[] inputCache = new float[5];
 	private Bird bird = null;
-
-	public BasicBrain Brain { get; set; }
 
 	public void AssignBird(Bird bird)
 	{
 		this.bird = bird;
-		bird.Terminated += BirdTerminated;
 	}
 
 	public void Think()
 	{
-		var pipe = Pipes.Peek();
-
-		inputCache[0] = (bird.transform.position.y - 25.5f) / 9f;
-		inputCache[1] = bird.body.velocity.y / 10f;
-
-		if (pipe != null)
-		{
-			inputCache[2] = (pipe.transform.position.x - 20.5f) / 14f;
-			inputCache[3] = (pipe.UpperY - 25.5f) / 9f;
-			inputCache[4] = (pipe.LowerY - 25.5f) / 9f;
-		}
-		else
-		{
-			inputCache[2] = 1f;
-			inputCache[3] = 1f;
-			inputCache[4] = 0f;
-		}
-
-		Brain.NeuralNetwork.Input.Transfer(inputCache);
-		Brain.NeuralNetwork.Eval();
-		var out1 = Brain.NeuralNetwork.Output[0u];
-		var out2 = Brain.NeuralNetwork.Output[1u];
-
-		if (out1 > out2) bird.Jump();
-	}
-
-	private void BirdTerminated()
-	{
-		var time = (float)bird.TimeSinceSpawned.TotalSeconds;
-		var reward = Mathf.Pow(time / 100f, 4f);
-
-		var pipe = Pipes.Peek();
-		var pipeCenter = (pipe.UpperY + pipe.LowerY) / 2f;
-		var penalty = Mathf.Pow((bird.transform.position.y - pipeCenter) / 100f, 4f);
-
-		Brain.EvolutionValue = Mathf.Max(reward - penalty, 1e-10f);
+		if (Random.Range(0, 100) < 2) bird.Jump();
 	}
 }
--- src/FlappyBirdAI/Assets/Scripts/Simulation.cs
+++ src/FlappyBirdAI/Assets/Scripts/Simulation.cs
@@ -1,56 +1,22 @@
-﻿using GrandIntelligence;
-
-public class Simulation
+﻿public class Simulation
 {
-	private Agent[] agents = null;
-	private DarwinBgea evolution = null;
-
 	public void Begin(Agent[] agents)
 	{
-		this.agents = agents;
 
-		var prototype = new NeuralBuilder(Shape.As2D(1u, 5u));
-		prototype.FCLayer(8u, ActivationFunction.ELU);
-		prototype.FCLayer(2u, ActivationFunction.Sigmoid);
-
-		var firstGen = new Population((uint)agents.Length);
-		for (var i = 0; i < agents.Length; ++i)
-		{
-			var brain = new BasicBrain(prototype);
-			brain.Randomize(-1.0f, 1.0f, Distribution.Uniform);
-			firstGen.Add(brain);
-		}
-
-		evolution = new DarwinBgea
-		(
-			firstGen,
-			Selection.RandFit(1u),
-			BasicBrain.SequentialEvenCrossover(firstGen.Size, ((BasicBrain)firstGen[0u]).NeuralNetwork.Params),
-			generations: 1000u,
-			mutation: 15.0f
-		);
-
-		prototype.Dispose();
 	}
 
 	public void EpisodeStart()
 	{
-		for (var i = 0; i < agents.Length; ++i)
-		{
-			agents[i].Brain = (BasicBrain)evolution.Generation[(uint)i];
-		}
+
 	}
 
 	public void BirdTerminated(int birdsLeft)
 	{
-		if (birdsLeft == 0)
-		{
-			evolution.Cycle();
-		}
+
 	}
 
 	public void End()
 	{
-		evolution?.Dispose();
+
 	}
 }
